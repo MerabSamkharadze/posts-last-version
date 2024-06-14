29f3ce2035027f476bd4d5d0665903b2f973a5ba
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-14 20:20:17 +0400

    add ajaxFncPosts dinamic function for fetch any url and do any fnc

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict ";
+const posts_container = document.querySelector(".posts_container");
+
+function ajaxFncPosts(url, callback) {
+  fetch(url, { method: "GET" })
+    .then((responseInfo) => {
+      if (!responseInfo.ok) throw responseInfo.status;
+      return responseInfo.json();
+    })
+    .then((responseData) => {
+      callback(responseData);
+    })
+    .catch((error) => {
+      const h1 = document.createElement("h1");
+      h1.innerText = "Server Problem 😣";
+      posts_container.appendChild(h1);
+    });
+}
